417bc6e06fc362e1aa6d1d970b3b935fb4bfd1ff
Artem Ulko <ulkoart@gmail.com>
2020-06-15 10:46:44 +0300

    delete task by swipe

--- ToDoList/TableViewController.swift
+++ ToDoList/TableViewController.swift
@@ -69,23 +69,31 @@ class TableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+//        let context = getContext()
+//        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+//
+//        if let objects = try? context.fetch(fetchRequest) {
+//            for object in objects {
+//                context.delete(object)
+//            }
+//        }
+//
+//        do {
+//            try context.save()
+//        } catch let error as NSError {
+//            print(error.localizedDescription)
+//        }
+        
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return tasks.count
     }
 
@@ -95,6 +103,36 @@ class TableViewController: UITableViewController {
         let task = tasks[indexPath.row]
         cell.textLabel?.text = task.title
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let task = tasks[indexPath.row]
+            let context = getContext()
+            
+            let predicate = NSPredicate(format: "SELF == %@", task.objectID)
+            let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+            fetchRequest.predicate = predicate
+
+            do {
+                let fetchedEntities = try context.fetch(fetchRequest)
+                if let entityToDelete = fetchedEntities.first {
+                    context.delete(entityToDelete)
+                    tasks.remove(at: indexPath.row)
+                    self.tableView.reloadData()
+                }
+            } catch {
+                print(error.localizedDescription)
+            }
+
+            do {
+                try context.save()
+            } catch {
+                print(error.localizedDescription)
+            }
+
+            
+        }
     }
 
 }
